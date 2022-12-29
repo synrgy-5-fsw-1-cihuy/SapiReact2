8933839d3e451f45c7b9d09ac6110ca8b8af8a9b
helmis14 <sulaemanfc48@gmail.com>
2022-12-29 19:20:33 +0700

    setup error jest

---
+++ src/dist/App.test.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _react = require("@testing-library/react");
+
+var _App = _interopRequireDefault(require("./App"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+test("renders the landing page", function () {});
--- src/dist/setupTests.dev.js
+++ src/dist/setupTests.dev.js
@@ -1,19 +1,25 @@
+"use strict";
+
+require("@testing-library/jest-dom");
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
-
 Object.defineProperty(window, 'matchMedia', {
-    writable: true,
-    value: jest.fn().mockImplementation(query => ({
+  writable: true,
+  value: jest.fn().mockImplementation(function (query) {
+    return {
       matches: false,
       media: query,
       onchange: null,
-      addListener: jest.fn(), // deprecated
-      removeListener: jest.fn(), // deprecated
+      addListener: jest.fn(),
+      // deprecated
+      removeListener: jest.fn(),
+      // deprecated
       addEventListener: jest.fn(),
       removeEventListener: jest.fn(),
-      dispatchEvent: jest.fn(),
-    })),
-  });
+      dispatchEvent: jest.fn()
+    };
+  })
+});
